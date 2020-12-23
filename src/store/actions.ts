acb7d6002570689f6dd4807b98ff51bf334c3d18
liuduan <liuduan@fenbi.com>
2020-12-23 21:14:54 +0800

    update

--- src/store/actions.ts
+++
@@ -1,9 +0,0 @@
-import { DECREMENT, INCREMENT } from "./action-types";
-
-export const decrement = () => ({
-    type: DECREMENT,
-});
-
-export const increment = () => ({
-    type: INCREMENT,
-});
